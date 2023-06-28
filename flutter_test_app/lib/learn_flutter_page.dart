6eea527f2f5179d51b1674482fa96469707367bb
SaahBrice <saahbrice98@gmail.com>
2023-06-28 12:45:24 +0200

    Flutter app test create widgets

--- flutter_test_app/lib/learn_flutter_page.dart
+++ flutter_test_app/lib/learn_flutter_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class LearnFlutterPgae extends StatefulWidget {
   const LearnFlutterPgae({super.key});
 
@@ -13,14 +12,38 @@ class _LearnFlutterPgaeState extends State<LearnFlutterPgae> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('learning flutter'),
-        leading: IconButton(onPressed: (){
-          Navigator.of(context).pop();
-        }, icon:  const Icon(Icons.arrow_back_ios)),
+        title: const Text('learning flutter'),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(Icons.arrow_back_ios)),
       ),
-      body: const Column(
+      body: Column(
         children: [
-          Image.asset('image/image1.jpg')
+          Image.asset('images/image1.jpg'),
+          const SizedBox(
+            height: 20,
+          ),
+          const Divider(
+            height: 20,
+            color: Colors.red,
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
+            color: Colors.grey,
+            width: double.infinity,
+            child: const Center(
+              child: Text(
+                'This is my text',
+                style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+              ),
+            ),
+          ),
+          ElevatedButton(onPressed: (){print('Elevated button pressed');}, child: const Text('Elevated button')),
+          OutlinedButton(onPressed: (){print('Elevated button pressed');}, child: const Text('Elevated button')),
+          TextButton(onPressed: (){print('Elevated button pressed');}, child: const Text('Elevated button')),
         ],
       ),
     );
